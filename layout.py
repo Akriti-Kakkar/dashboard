db465584447ccad7ff48082bcb0c546ef58d0bfd
Akriti-Kakkar <Akriti.Kakkar@httsfund.com>
2024-08-12 18:09:19 +0530

    Update layout.py

--- layout.py
+++ layout.py
@@ -1,17 +1,20 @@
 import streamlit as st
 
+st.set_page_config(page_title='Dashboard', page_icon='🌎', layout="wide",
+                           initial_sidebar_state="expanded")
+
+st.header("Design 1: collapsable metric window for each row, highlighting features of the metric in its name ")
 major_col, major_col1 = st.columns([0.99, 0.01])
-with st.expander('Metrics', expanded=True):
+with st.expander('Winning Stats For The Year 2022', expanded=True):
     col1, col2, col3 = st.columns(3)
     with col1:
         st.metric('metric1', 1000000)
     with col2:
         st.metric('metric2', 1000000)
     with col3:
-        st.metric('metric3', 100000000000)
+        st.metric('metric3', 10000000000)
         
-import streamlit as st
-
+st.header("Design 2: simple side by side html view of metrics in mobile and laptop view")
 # HTML and CSS for side-by-side metrics
 html_code = """
 <div style="display: flex; justify-content: space-around;">
