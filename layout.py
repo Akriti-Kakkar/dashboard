c6d54c8ccec7e4ce274fb905af7b73e000b8447c
Akriti-Kakkar <Akriti.Kakkar@httsfund.com>
2024-08-12 17:00:39 +0530

    Create layout.py

---
+++ layout.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+major_col, major_col1 = st.columns([0.99, 0.01])
+with major_col:
+    col1, col2, col3 = st.columns(3)
+    with col1:
+        st.metric('metric1', 1000000)
+    with col2:
+        st.metric('metric2', 1000000)
+    with col3:
+        st.metric('metric3', 100000000000)
