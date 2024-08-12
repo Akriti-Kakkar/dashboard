53b99ba53dc744dcb1771afbfef4d9f9e7b1d8b1
Akriti-Kakkar <Akriti.Kakkar@httsfund.com>
2024-08-12 17:53:09 +0530

    Update layout.py

--- layout.py
+++ layout.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 major_col, major_col1 = st.columns([0.99, 0.01])
-with major_col:
+with st.expander('Metrics', expanded=True):
     col1, col2, col3 = st.columns(3)
     with col1:
         st.metric('metric1', 1000000)
@@ -9,3 +9,4 @@ with major_col:
         st.metric('metric2', 1000000)
     with col3:
         st.metric('metric3', 100000000000)
+        
